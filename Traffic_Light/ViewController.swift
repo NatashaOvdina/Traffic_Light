ba02d24719982b699362d3a8b9bb3e9136c4e171
NatashaOvdina <natashaovdina@yahoo.com>
2023-11-05 00:57:38 +0300

    Put a button on a screen & make rounded corners. Made views with lower alpha.

--- Traffic_Light/ViewController.swift
+++ Traffic_Light/ViewController.swift
@@ -12,12 +12,14 @@ class ViewController: UIViewController {
     @IBOutlet var viewRed: UIView!
     @IBOutlet var viewYellow: UIView!
     @IBOutlet var viewGreen: UIView!
+    @IBOutlet var button: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         viewRed.layer.cornerRadius = 50
         viewYellow.layer.cornerRadius = 50
         viewGreen.layer.cornerRadius = 50
+        button.layer.cornerRadius = 10
     }
 
 
