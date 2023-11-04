a8dfcf15cc78f68c39dac1d96acfd43b3859a8ac
NatashaOvdina <natashaovdina@yahoo.com>
2023-11-05 00:27:39 +0300

    Initial Commit

---
+++ Traffic_Light/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Traffic_Light
+//
+//  Created by Natalia Ovdina on 05.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
